c2cf9df371cfaedb7ddead18bf2103cbe9daf070
Yuriy.Khilko <yurakhilko@gmail.com>
2024-05-14 18:24:00 +0300

    first Kotlin program solution.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Enter your city…")
+    val city = readln()
+    println("User lives in $city")
+}
